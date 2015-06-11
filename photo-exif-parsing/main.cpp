c848ff4fd28bbdc1fcbc5a329d4e4c7a34008e4f
Tyler Powers <gr4yscale@gmail.com>
2015-06-11 16:10:47 +0100

    barebones project, use easyexif library for exif metadata parsing and jsonCPP to make generating json easier

---
+++ photo-exif-parsing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  photo-exif-parsing
+//
+//  Created by Tyler Powers on 6/11/15.
+//  Copyright (c) 2015 Tyler Powers. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    return 0;
+}
